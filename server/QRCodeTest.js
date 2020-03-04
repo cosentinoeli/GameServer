4b41fe718cae6e3c771b530f4786f24cc3bdd533
Michael Taylor <47544533+mike-taylor99@users.noreply.github.com>
2020-03-04 11:56:45 -0500

    Add QR Code to Electron App

--- server/QRCodeTest.js
+++ server/QRCodeTest.js
@@ -8,5 +8,12 @@ let address = 'http://' + serverIP.toString() + ':' + port.toString()
 
 /* log QRCode version of address */
 QRCode.toString(address, {type:'terminal'}, function (err, url) {
-    console.log(url)
+    console.log(typeof(url))
+    //document.body.appendChild(url)
 })
+
+var canvas = document.getElementById('canvas');
+QRCode.toCanvas(canvas, address, function (error) {
+    if (error) console.error(error)
+    console.log('success!');
+  })
